ec00c904f9f59458cc347b39a96406e4e921754d
SanaUllah-Gondal <sanaullahgondal87@gmail.com>
2025-04-30 09:18:23 +0500

    Commit

--- generated_plugin_registrant.cc
+++ generated_plugin_registrant.cc
@@ -6,9 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_selector_windows/file_selector_windows.h>
+#include <file_selector_linux/file_selector_plugin.h>
 
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileSelectorWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
+  file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
 }
